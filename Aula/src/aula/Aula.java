5f9436a1838fd59d553aedba36659a18316f9777
Tecco2233 <biel.tecco@gmail.com>
2019-06-25 12:14:07 -0300

    duhsauhdsauhdsauh

---
+++ Aula/src/aula/Aula.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Aula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char a = 'a';
+        int i = 0;
+        float f = 10.3f;
+        double d = 10;
+        
+        System.out.println("a = " + a);
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+    }
+    
+}
